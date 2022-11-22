dc2cdbdf918ffdc2826637761009ff417e66e203
Jethro Bizumuremyi <jethrobizumuremyi@my.unt.edu>
2022-11-22 09:05:33 -0600

    color contrast part 2, minor ui fixes

--- src/Pages/LoggedOut/Catalog.tsx
+++ src/Pages/LoggedOut/Catalog.tsx
@@ -23,22 +23,22 @@ const Catalog = () => {
                             </Typography>
                             <ul className="nav flex-column">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                                    
                                         <span data-feather="file" className="align-text-bottom"></span>
                                         <a href = "https://en.wikipedia.org/wiki/Accessibility" > Introduction to Accessibility </a>
-                                    </a>
+                                    
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                                 
                                         <span data-feather="file" className="align-text-bottom"></span>
                                         <a href = "https://www.w3.org/WAI/fundamentals/accessibility-intro/" > Introduction to Web Accessibility </a>
-                                    </a>
+                                   
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                                   
                                         <span data-feather="shopping-cart" className="align-text-bottom"></span>
                                       <a href = "https://www.w3.org/WAI/fundamentals/accessibility-intro/#what" > Accessibility fundamentals </a>                                 
-                                    </a>
+                                    
                                 </li>    
                             </ul>
                         </Grid>
@@ -49,34 +49,34 @@ const Catalog = () => {
                             </Typography>
                             <ul className="nav flex-column">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                               
                                         <span data-feather="file" className="align-text-bottom"></span>
                                         <a href = "/Perceivable/#perceive_intro" > What is Perceivable </a>
-                                    </a>
+                                   
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                            
                                         <span data-feather="shopping-cart" className="align-text-bottom"></span>
                                         <a href = "/Perceivable/#perceive_text_option" > Perceivable text options </a>                                    
-                                    </a>
+                              
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                             
                                         <span data-feather="users" className="align-text-bottom"></span>
                                         <a href = "/Perceivable/#perceive_nontext_options" > Perceivable non-text options </a>                                    
-                                    </a>
+                                
                                 </li>     
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                              
                                         <span data-feather="users" className="align-text-bottom"></span>
                                         <a href = "/Perceivable/#perceive_text_alts" > Perceivable text alternatives </a>                                    
-                                    </a>
+                                   
                                 </li>   
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                             
                                         <span data-feather="users" className="align-text-bottom"></span>
                                         <a href = "/Perceivable/#perceive_distinguish" > Making text more distinguishable </a>                                    
-                                    </a>
+                             
                                 </li>   
                             </ul>
                         </Grid>
@@ -87,22 +87,22 @@ const Catalog = () => {
                             </Typography>
                             <ul className="nav flex-column">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                      
                                         <span data-feather="file" className="align-text-bottom"></span>
                                         <a href = "/Operable/#operable_intro" > What is Operable </a>                                    
-                                    </a>
+                         
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                             
                                         <span data-feather="shopping-cart" className="align-text-bottom"></span>
                                         <a href = "/Operable/#operable_standardIO" > Standard input/output </a>                                                                       
-                                    </a>
+                             
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                             
                                         <span data-feather="users" className="align-text-bottom"></span>
                                         <a href = "/Operable/#operable_nagivation" > Helpful website navigation steps </a>                                                                      
-                                    </a>
+                         
                                 </li>      
                             </ul>
                         </Grid>
@@ -113,22 +113,22 @@ const Catalog = () => {
                             </Typography>
                             <ul className="nav flex-column">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                  
                                         <span data-feather="file" className="align-text-bottom"></span>
                                         <a href = "/Understandable/#understandable_intro" > What is Understandable </a>                                                                     
-                                    </a>
+                                
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                             
                                         <span data-feather="shopping-cart" className="align-text-bottom"></span>
                                         <a href = "/Understandable/#understandable_readability" > Improve readability </a>                                                                                                       
-                                    </a>
+                               
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                        
                                         <span data-feather="users" className="align-text-bottom"></span>
                                         <a href = "/Understandable/#understandable_input_assistance" > Input Assistance </a>                                                                                                      
-                                    </a>
+                                    
                                 </li>      
                             </ul>
                         </Grid>
@@ -139,16 +139,16 @@ const Catalog = () => {
                             </Typography>
                             <ul className="nav flex-column">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                           
                                         <span data-feather="file" className="align-text-bottom"></span>
                                         <a href = "/Robust/#robust_intro" > What is Robust </a>                                                                                                     
-                                    </a>
+                            
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                            
                                         <span data-feather="shopping-cart" className="align-text-bottom"></span>
                                         <a href = "/Robust/#robust_list_of_tech" > Lists of "most used" tech   </a>                                                                                                                                      
-                                    </a>
+                                    
                                 </li>   
                             </ul>
                         </Grid>
